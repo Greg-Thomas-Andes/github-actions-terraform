6d6e7ca703b393b3bfbe18661e10777b82215b74
Greg Thomas Andes <andesgregthomas@gmail.com>
2024-08-15 16:17:25 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,34 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "your-s3-bucket-name"
+    key            = "path/to/your/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-test-deleteLater"
+    encrypt        = true
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-southeast-1"
 }
 
-# Create a VPC
-# resource "aws_vpc" "example" {
-#   cidr_block = "10.0.0.0/16"
-# }
-
-#Terraform module to create AWS VPC resources 🇺🇦
+# Terraform module to create AWS VPC resources
 module "gtavpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "gta-vpc"
   cidr = "172.16.0.0/16" # or any other valid private range
 
   azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  private_subnets = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  private_subnets = ["172.16.1.0/24", "172.16.2.0/24"]
   public_subnets  = ["172.16.101.0/24", "172.16.102.0/24", "172.16.103.0/24"]
 
   enable_nat_gateway = true
