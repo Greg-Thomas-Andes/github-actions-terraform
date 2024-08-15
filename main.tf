42f6d7fc9e4a5b13be231258a6cc91b38c4b4248
Greg Thomas Andes <andesgregthomas@gmail.com>
2024-08-15 16:52:47 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "gtavpc" {
   cidr = "172.16.0.0/16" # or any other valid private range
 
   azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  private_subnets = ["172.16.1.0/24", "172.16.2.0/24"]
+  private_subnets = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
   public_subnets  = ["172.16.101.0/24", "172.16.102.0/24", "172.16.103.0/24"]
 
   enable_nat_gateway = true
